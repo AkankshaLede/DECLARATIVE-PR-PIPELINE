9a7fe6d6da4bf288460752a3567ccbae8de32700
akanksha <akanksha.lede@moschip.com>
2025-05-22 13:54:33 +0530

    changes in values to test

--- tests/test_calculator_logic.py
+++ tests/test_calculator_logic.py
@@ -7,11 +7,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from calculator import calculator
 
 def test_add_positive():
-    result = calculator(16, -80, ['add'])
+    result = calculator(16, 90, ['add'])
     assert result['add'] > 0, "Add result should be positive"
 
 def test_subtract_positive():
-    result = calculator(0, 7, ['subtract'])
+    result = calculator(100, 7, ['subtract'])
     assert result['subtract'] > 0, "Subtract result should be positive"
 
 def test_divide_greater_equal_one():
